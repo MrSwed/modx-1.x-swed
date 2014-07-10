26c4e4063b2a57c3ffa26e28df069fa352ff0896
MrSwed <webmaster@collection.com.ua>
2014-07-10 17:44:00 +0400

    PHx dateformat modifier. Convert date from date string for TV.

--- assets/plugins/phx/modifiers/dateformat.phx.php
+++ assets/plugins/phx/modifiers/dateformat.phx.php
@@ -16,10 +16,8 @@ $options = explode("|",$options);
 if (!is_numeric($output)) {
  if (empty($options[1])) $options[1] = "%d-%m-%Y %H:%M:%S";
  $parsed = strptime($output,$options[1]);
-// $debug.= " get from strptime($output,{$options[1]}) ".var_export($parsed,1);
  $output = mktime($parsed['tm_hour'],$parsed['tm_min'],$parsed['tm_sec'],$parsed['tm_mon']+1,$parsed['tm_mday'],$parsed['tm_year']+1900);
 }
-//$debug .= " try strftime({$options[0]},$output)";
-return strftime($options[0],$output);//. " <b style='font-size:10px;'>$debug</b>";
+return strftime($options[0],$output);
 
 ?>

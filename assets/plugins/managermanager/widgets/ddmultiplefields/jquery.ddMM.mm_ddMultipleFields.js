7aa8b0600155d75a02208a75c5200308c9f436b3
MrSwed <webmaster@sdcollection.com>
2016-03-15 10:01:22 +0300

    update MM https://github.com/MrSwed/MODXEvo.plugin.ManagerManager downgrade ddtools - it is bad practic set own email address as default from field https://github.com/DivanDesign/MODXEvo.library.ddTools/issues/4

--- assets/plugins/managermanager/widgets/ddmultiplefields/jquery.ddMM.mm_ddMultipleFields.js
+++ assets/plugins/managermanager/widgets/ddmultiplefields/jquery.ddMM.mm_ddMultipleFields.js
@@ -368,6 +368,10 @@ $.ddMM.mm_ddMultipleFields = {
 				case 'select':
 				_this.makeSelect(val[key], _cTitle, _inst.coloumnsData[key], _inst.colWidth[key], $col);
 				break;
+			//Если мультиселект
+				case 'multiselect':
+				_this.makeSelect(val[key], _cTitle, _inst.coloumnsData[key], _inst.colWidth[key], $col,true);
+				break;
 			//Если дата
 				case 'date':
 				_this.makeDate(val[key], _cTitle, $col);
@@ -596,8 +600,8 @@ $.ddMM.mm_ddMultipleFields = {
 		});
 	},
 	//Функция создания списка
-	makeSelect: function(value, title, data, width, $fieldCol){
-		var $select = $('<select class="ddField" style="' + this._united(width,"width: %s") + '">');
+	makeSelect: function(value, title, data, width, $fieldCol, $multiple){
+		var $select = $('<select class="ddField" style="' + this._united(width,"width: %s") + '"' + ($multiple?' multiple size="" ':'') + '>');
 		
 		if (data){
 			var dataMas = $.parseJSON(data),
@@ -610,7 +614,7 @@ $.ddMM.mm_ddMultipleFields = {
 			$select.append(options);
 		}
 		
-		if (value){$select.val(value);}
+		if (value) {$select.val($multiple?value.split(","):value);}
 		
 		return $select.appendTo($fieldCol);
 	},

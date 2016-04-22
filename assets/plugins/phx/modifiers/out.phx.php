c1639f141a6ce66755307590a008986d1143ce12
MrSwed <webmaster@sdcollection.com>
2016-04-22 15:44:59 +0300

    [+] phx modifier

---
+++ assets/plugins/phx/modifiers/out.phx.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+ * description: Output special as printf if not empty 
+ *  
+ * usage: [+docid:out=`<div class="wrap">%s</div>`+]
+ * where %s will be replaced to output result
+ *
+ * author MrSwed <webmaster@sdcollection.com>
+*/
+
+$options = empty($options)?"%s":$options;
+return $output?sprintf($options,$output):$output;

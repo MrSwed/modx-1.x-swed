cd67815aa6c096d0c41f2f795ed558136c729104
MrSwed <webmaster@sdcollection.com>
2016-08-08 15:01:08 +0300

    ShowBlock v.0.3 [-] required getInheritField, now it integrated

--- assets/element/snippet/Template/ShowBlock.php
+++ assets/element/snippet/Template/ShowBlock.php
@@ -4,14 +4,14 @@
 	*
 	* Show the contents of the block specified by the user depending on the placement and inheritance
 	*
-	* @category    snippet
-	* @version     0.2
-	* @license     http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
-	* @internal    @properties
-	* @internal    @modx_category Content
-	* @internal    @installset base, sample
-	* @internal    @author MrSwed webmaster@sdcollection.com
-	* @internal    @requirements [[ddGetMultipleField]] [[getInheritField]]
+	* @category      snippet
+	* @version       0.3
+	* @license       http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
+	* @internal      @properties
+	* @internal      @modx_category Content
+	* @internal      @installset base, sample
+	* @author        MrSwed webmaster@sdcollection.com
+	* @requirements  Snippet ddGetMultipleField https://github.com/MrSwed/MODXEvo.snippet.ddGetMultipleField
 	*/
 
 /* mm_rules example :
@@ -29,7 +29,7 @@ $p = array(
 	)),
 	"sel_inheritance" => json_encode(array(
 		array("", ""), // use parent setting
-		array(0, "Нет"),
+		array(0, "Нет, отменить наследование"),
 		array(1, "Для всех"),
 		array(2, "Только для дочерних"),
 		array(3, "Для контейнеров"),
@@ -52,7 +52,7 @@ mm_ddMultipleFields("blocks", '', '', 'richtext,select,select', 'Текст,Ме
                
 		sel_inheritance - Настройки наследования             
 		  не задано -  используются значения родителя, т.е. заданное значение будет отображено только для текухего документа, дочерние покажут значения родителя
-		      0     -  "Нет". Отменить наследование всего блока
+		      0     -  "Нет, отменить наследование". Достаточно указать для одного одноименного блока, при условии что для остальных ничего не задано, иначе будут действовать другие условия наследования
 		      1     -  "Для всех". Все типы дочерних наследуют указанный блок
 		      2     -  "Только для дочерних". В текущем документе блок показан не будет
 		      3     -  "Для контейнеров". Если текущий - контейнер, то в нем и в дочерних контейнерах блок будет отображен 
@@ -70,6 +70,8 @@ if (empty($name)) return ""; // block name is required
 $id = isset($id) ? $id : $modx->documentObject['id'];
 $tv = isset($tv) ? $tv : "blocks";
 $out = isset($out) ? $out : "<div class='$name'>%s</div>";
+$rootID = isset($rootID) ? $rootID : 0;
+$depth = isset($depth) ? $depth : 100;
 
 if ($id == $modx->documentObject['id']) $docData = $modx->documentObject;
 else $docData = $modx->getDocument($id, "parent,isfolder");
@@ -78,12 +80,15 @@ else $docData = $modx->getDocument($id, "parent,isfolder");
 $value = $modx->evalSnippets("[[ddGetMultipleField? &docField='$tv' &filter='1::$name||2::||2::0||2::1".((int)$docData["isfolder"] ? "||2::3" : "")."' &columns='0']]");
 
 // Если для текущего ничего нет, ищем родительские с разрешенным наследованием
-// todo: избавиться от двойного вызова getInheritField
-if (empty($value) and "0"!=$modx->evalSnippets("[[getInheritField? &id=`{$docData["parent"]}` &field=`$tv` &runSnippet=`ddGetMultipleField? &string='%s'  &filter='1::$name||2::0||2::1||2::2".((int)$docData["isfolder"] ? "||2::3||2::4" : "||2::5")."' &columns='2'`]]")) {
-	
-	$value = $modx->evalSnippets("[[getInheritField? &id=`{$docData["parent"]}` &field=`$tv` &runSnippet=`ddGetMultipleField? &string='%s'  &filter='1::$name||2::1||2::2".((int)$docData["isfolder"] ? "||2::3||2::4" : "||2::5")."' &columns='0'`]]");
+$cid = $id;
+while (empty($value) and (int)$cid != $rootID and --$depth > 0) {
+	$value = $modx->getTemplateVarOutput(array($tv, "parent"), $cid, 1);
+	$cid = $value["parent"];
+	$value = $value[$tv];
+	if ("0" == $modx->evalSnippets("[[ddGetMultipleField? &string='$value'  &filter='1::$name||2::0||2::1||2::2".((int)$docData["isfolder"] ? "||2::3||2::4" : "||2::5")."' &columns='2']]")) {
+		$value = ""; // Наследование отменено
+		break;
+	} else $value = $modx->evalSnippets("[[ddGetMultipleField? &string='$value'  &filter='1::$name||2::1||2::2".((int)$docData["isfolder"] ? "||2::3||2::4" : "||2::5")."' &columns='0']]");
 }
-
 return $value ? ($out ? sprintf($out, $value) : "") : "";
-
 ?>

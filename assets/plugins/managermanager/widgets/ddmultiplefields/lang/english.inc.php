6d70b174c3ec3138c9afa119a93742e4483924c7
MrSwed <webmaster@sdcollection.com>
2017-05-03 15:51:34 +0300

    [*] restore MM (SD)

---
+++ assets/plugins/managermanager/widgets/ddmultiplefields/lang/english.inc.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * MODX ManagerManager mm_ddMultipleFields language file
+ *
+ * @version 4.6-SD (2017-01-31)
+ * @author MrSwed
+ *
+ * @language English
+ * @package modx
+ * @subpackage ManagerManager.mm_ddMultipleFields
+ *
+ */
+
+$_lang = array_merge(@$_lang,array(
+	"batch_filling" => "Batch Filling",
+	"clone_row" => "Clone row",
+	"raw_btn_tip"=>"Show/hide original field. Available copying and pasting. After insertion, press the reset button. Attention! Wrong original field format can result in unpredictable consequences!"
+	
+));
+?>
---
+++ assets/plugins/managermanager/widgets/ddmultiplefields/lang/russian-UTF8.inc.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * MODX ManagerManager mm_ddMultipleFields language file
+ *
+ * @version 4.6-SD (2017-01-31)
+ * @author MrSwed
+ *
+ * @language Russian-UTF8
+ * @package modx
+ * @subpackage ManagerManager.mm_ddMultipleFields
+ *
+ */
+
+$_lang = array_merge(@$_lang,array(
+	"batch_filling" => "Пакетное заполнение",
+	"clone_row" => "Клонировать строку",
+	"raw_btn_tip"=>"Показать/Скрыть оригинальное поле. Доступны стандартные копирование и вставка. После вставки, нажмите кнопку сброса. Внимание! Неправильный формат исходного поля может привести к непредсказуемым последствиям!",
+));
+?>
